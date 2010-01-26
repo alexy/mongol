01ad3c424dc31e3187dae5d6496131edd0077bb7
Alexy Khrabrov <deliverable@gmail.com>
2010-01-25 22:09:23 -0500

    drank rocket

--- src/main/clojure/com/tfitter/dyn/rankrank.clj
+++ src/main/clojure/com/tfitter/dyn/rankrank.clj
@@ -2,7 +2,7 @@
 ;; by the position of the rank in its decreasing order
 
 ;;  this in fact slows things down
-(defn ranked-days-transients
+(defn ranked-days-nested-transients
   "replace rank for each day by its position in order, desc"
   [dpr]
     (->> dpr (reduce (fn [res [user days]] 
@@ -21,22 +21,29 @@
     ))
     
 (defn ranked-days
-  "replace rank for each day by its position in order, desc"
-  [dpr]
-    (->> dpr (reduce (fn [res [user days]] 
-      (reduce (fn [res [day num]] 
-        (update-in res [day] #(conj (or % []) [user num]))) 
-      res days)) {}) 
-      (map (fn [[k v]] 
+  "replace rank for each day by its position in order, desc
+  TODO add progress"
+  [dpr & [quant]]
+  (let [quant (or quant 1000000)
+    [unsorted _] (->> dpr (reduce (fn [[res progress] [user days]] 
+      (reduce (fn [[res progress] [day num]]
+        (when (and quant (= (mod progress quant) 0)) 
+			       (.print System/err (str " " (quot progress quant))))
+        (let [res (update-in res [day] #(conj (or % []) [user num]))]
+          [res (inc progress)])) 
+      res days)) [{} 0]))]
+      (.print System/err " days: ") 
+      (->> unsorted (map (fn [[k v]] 
         (let [
           with-nums-desc (sort-by second > v)
           ranked (map (fn [[user _] rank] [user rank]) 
              with-nums-desc (iterate inc 0))
           ]
+        (.print System/err (str " " k))
         [k ranked])))
     ;; (apply concat) (apply hash-map)
     (into {})
-    ))
+    )))
 
 ;; http://gist.github.com/283450    
 (defn assoc-in-with
@@ -47,7 +54,7 @@
     (assoc m k v)))
     
     
-(defn ranked-graph
+(defn ranked-graph-simple
   "takes the result of ranked-graph"
   [by-day]        
   (->> by-day (reduce (fn [res [day pairs]]
@@ -55,3 +62,20 @@
         (assoc-in-with res (sorted-map) [user day] rank))
         res pairs)) {})))
   
+(defn ranked-graph
+  "takes the result of ranked-graph"
+  [by-day & [quant]]
+  (let [quant (or quant 1000000)]        
+  (->> by-day (reduce (fn [[res progress] [day pairs]]
+      (reduce (fn [[res progress] [user rank]]
+        (let [rx (res user)
+          yz (assoc (or rx (sorted-map)) day rank)
+          ]
+        (when (and quant (= (mod progress quant) 0)) 
+			       (.print System/err (str " " (quot progress quant))))        
+        (let [res (assoc! res user yz)]
+          [res (inc progress)])))
+        res pairs)) [(transient {}) 0])
+        first
+        persistent!
+        )))
